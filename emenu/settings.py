0bf9a75c7c380fea3e82a87b67fe516ff08928ff
Ridzen <ridze00@gmail.com>
2022-07-31 13:30:15 -0500

    added requirements for deploing

--- emenu/settings.py
+++ emenu/settings.py
@@ -41,7 +41,10 @@ INSTALLED_APPS = [
     # libraries
     'rest_framework',
     'ckeditor',
+    'ckeditor_uploader',
     'django_filters',
+    'corsheaders',
+    'drf_yasg',
 
     # my apps
     'apps.foodcards',
@@ -80,6 +83,27 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'emenu.wsgi.application'
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://7856-212-42-120-155.in.ngrok.io',
+    "http://127.0.0.1:8080"
+]
+# If this is used, then not need to use CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'https://7856-212-42-120-155.in.ngrok.io',
+    "http://127.0.0.1:8080"
+]
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
