eab5216f372baeaf8174bf7fd18e83939f916188
Ridzen <ridze00@gmail.com>
2022-08-01 00:51:50 -0500

    changed procfile

--- emenu/urls.py
+++ emenu/urls.py
@@ -14,33 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
+from django.urls import path, include
+from rest_framework_swagger.views import get_swagger_view
 from apps.foodcards import urls
+from .yasg import urlpatterns as doc_urls
+from . import settings
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="First Api",
-        default_version='v1',
-        description="RESTfull app for provide API for mobile @ web-front applications",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
-
+scheme_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
-
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-
     path('admin/', admin.site.urls),
 
     path('api/v1/foodcards', include('apps.foodcards.urls')),
